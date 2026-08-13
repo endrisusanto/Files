7ead0ed943d3b4b418f72f36e6b0e537c3fe6c2b
endri Susanto <endrisusantosekarang@gmail.com>
2026-08-13 08:08:57 +0700

    fix: add missing MotionEvent import to fix apk compilation

--- android/app/src/main/java/com/example/bridge/MainActivity.kt
+++ android/app/src/main/java/com/example/bridge/MainActivity.kt
@@ -18,6 +18,7 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.view.Gravity
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
