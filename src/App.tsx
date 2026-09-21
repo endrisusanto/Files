0c3ddce61f2dea76c00cd8003f9b5faec38a7fc3
endri Susanto <endrisusantosekarang@gmail.com>
2026-09-21 10:16:31 +0700

    chore: release v0.1.184

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { listen } from "@tauri-apps/api/event";
 import logo from "./logo.svg";
 import { UpdateModal } from "./components/UpdateModal";
 
-const APP_VERSION = "0.1.183";
+const APP_VERSION = "0.1.184";
 
 type Device = {
   id: string;
